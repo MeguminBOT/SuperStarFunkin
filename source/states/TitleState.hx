0e24827a7088b22fd64a250c8f0fac3f1df3fbde
MeguminBOT <discord.meguminbot@gmail.com>
2024-06-28 17:53:20 +0200

    Oops! Deleted too much from TitleState

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -9,6 +9,10 @@ import states.MainMenuState;
 
 class TitleState extends MusicBeatState
 {
+	public static var muteKeys:Array<FlxKey> = [FlxKey.ZERO];
+	public static var volumeDownKeys:Array<FlxKey> = [FlxKey.NUMPADMINUS, FlxKey.MINUS];
+	public static var volumeUpKeys:Array<FlxKey> = [FlxKey.NUMPADPLUS, FlxKey.PLUS];
+
 	public static var initialized:Bool = false;
 	public static var closedState:Bool = false;
 	public static var updateVersion:String = '';
