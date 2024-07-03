7056500385c4364e01a1edb8726bca74424cf157
MeguminBOT <discord.meguminbot@gmail.com>
2024-07-03 13:16:31 +0200

    Credits script fix

--- example_mods/modpack_SuperStarFunkin/data/hyperstar/script_credits.lua
+++ example_mods/modpack_SuperStarFunkin/data/hyperstar/script_credits.lua
@@ -1,45 +1,41 @@
 function onCreatePost()
+	makeLuaText('header', 'Hyperstar:', 600, 1450, 425)
+	setTextSize('header', 36)
+	setTextColor('header', 'c0fffe')
+	setTextAlignment('header', 'left')
+	setObjectCamera('header', 'camOther')
 
-	makeLuaText('header', "SUPERSTAR FUNKIN':", 600, 1450, 425);
-	setTextSize('header', 50);
-	setTextColor('header', 'c0fffe');
-	setTextAlignment('header', 'left');
-	setObjectCamera('header', 'other');
+	makeLuaText('credits', '', 600, 1450, 475)
+	setTextSize('credits', 16)
+	setTextColor('credits', 'F6C0FF')
+	setTextAlignment('credits', 'left')
+	setObjectCamera('credits', 'other')
+	setTextString('credits', 'FrozenBlueberry - Glamrock Freddy Sprites, Background\nPouria_SFMs - Boyfriend Sprites\nPenove - Song\nZombie07King - Icons\nJustDom - Charting, Coding')
+	
+	makeLuaSprite('creditbg', '', 1440, 415)
+	makeGraphic('creditbg', 600, 180, '000000')
+	setProperty('creditbg.alpha', 0.5)
+	setObjectCamera('creditbg', 'camOther')
 
-	makeLuaText('credits', "FrozenBlueberry - Glamrock Freddy Sprites, Background\n\nPouria_SFMs - Boyfriend Sprites\n\nPenove - Song\n\nZombie07King - Icons\n\nJustDom - Charting, Coding", 600, 1450, 475);
-	setTextSize('credits', 25);
-	setTextColor('credits', 'f6c0ff');
-	setTextAlignment('credits', 'left');
-	setObjectCamera('credits', 'other');
+	addLuaSprite('creditbg', false)
+	addLuaText('header')
+	addLuaText('credits')
+end
 
-	makeLuaSprite('creditbg', "", 1440, 415);
-	makeGraphic('creditbg', 600, 600, '000000');
-	setProperty('creditbg.alpha', 0.5);
-	setObjectCamera('creditbg', 'other');
+function onBeatHit()
+	if curBeat == 4 then
+		doTweenX('header', 'header', 750, 2, 'elasticOut')
+		doTweenX('credits', 'credits', 750, 2, 'elasticOut')
+		doTweenX('creditbg', 'creditbg', 740, 2, 'elasticOut')
 
-	addLuaSprite('creditbg', false);
-	addLuaText('header');
-	addLuaText('credits');
-
-	runTimer('creditshow', 2, 1);
-
+		runTimer('credithide', 10, 1)
+	end
 end
 
 function onTimerCompleted(tag, loops, loopsLeft)
-
-	if tag == 'creditshow' then
-
-		doTweenX('header', 'header', 750, 1, 'quadOut');
-		doTweenX('credits', 'credits', 750, 1, 'quadOut');
-		doTweenX('creditbg', 'creditbg', 740, 1, 'quadOut');
-
-		runTimer('credithide', 11, 1);
-
-	elseif tag == 'credithide' then
-
-		doTweenX('header', 'header', 1450, 1, 'quadIn');
-		doTweenX('credits', 'credits', 1450, 1, 'quadIn');
-		doTweenX('creditbg', 'creditbg', 1440, 1, 'quadIn');
-
+	if tag == 'credithide' then
+		doTweenX('header', 'header', -1450, 2, 'elasticIn')
+		doTweenX('credits', 'credits', -1450, 2, 'elasticIn')
+		doTweenX('creditbg', 'creditbg', -1440, 2, 'elasticIn')
 	end
 end
